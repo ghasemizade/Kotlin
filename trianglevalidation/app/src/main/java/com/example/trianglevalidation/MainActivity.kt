cf1ac3c1266517d7ace7032411cc94f4a92e9806
Hosein <hosein@ghasemizade.com>
2022-05-19 19:27:44 +0430

    triangle valid

--- trianglevalidation/app/src/main/java/com/example/trianglevalidation/MainActivity.kt
+++ trianglevalidation/app/src/main/java/com/example/trianglevalidation/MainActivity.kt
@@ -3,6 +3,9 @@ package com.example.trianglevalidation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 
 
 class MainActivity : AppCompatActivity() {
@@ -10,21 +13,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val inputNumber = findViewById<TextView>(R.id.inputNumber)
+        val inputNumber1 = findViewById<TextView>(R.id.inputNumber1)
+        val inputNumber2 = findViewById<TextView>(R.id.inputNumber2)
+        val buttonShowname = findViewById<Button>(R.id.buttonShowname)
 
-        var sideA:Int = 6
-        var sideB:Int = 8
-        var sideC:Int = 10
 
+        buttonShowname.setOnClickListener(){
 
-        if (sideA + sideB > sideC && sideA + sideC > sideB && sideC + sideB > sideA)
-        {
-            var str = "this numbers create triangle :)"
-            Log.i("console", str.toString())
+            var sideA = inputNumber.text.toString().toInt()
+            var sideB = inputNumber.text.toString().toInt()
+            var sideC = inputNumber.text.toString().toInt()
+
+            if (sideA + sideB > sideC && sideA + sideC > sideB && sideC + sideB > sideA)
+            {
+                var str = "this numbers create triangle :)"
+                Toast.makeText(this,str , Toast.LENGTH_LONG).show()
+            }
+            else
+            {
+                var str2 = "this number s does not create triangle"
+                Toast.makeText(this, str2, Toast.LENGTH_LONG).show()
+            }
         }
-        else
-        {
-            var str2 = "this number s does not create triangle"
-            Log.i("console", str2.toString())
-        }
+
     }
 }
